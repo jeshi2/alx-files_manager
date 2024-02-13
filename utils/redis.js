d211a0ba534ae8ef05114843956d6781b6777de0
jeshi2 <jamesvean19@gmail.com>
2024-02-13 11:31:16 +0300

    redis

---
+++ utils/redis.js
@@ -0,0 +1,41 @@
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor () {
+    this.client = createClient({
+      host: 'localhost',
+      port: 6379
+    });
+
+    this.isClientConnected = true;
+
+    this.client.on('error', (error) => {
+      console.error('Redis error:', error);
+      this.isClientConnected = false;
+    });
+
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+  isAlive () {
+    return this.isClientConnected;
+  }
+
+  async get (key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set (key, value, durationInSeconds) {
+    return promisify(this.client.set).bind(this.client)(key, value, 'EX', durationInSeconds);
+  }
+
+  async del (key) {
+    return promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
