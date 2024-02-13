02ddfe5c4fe746dea972603b72c58275fb94b929
jeshi2 <jamesvean19@gmail.com>
2024-02-13 22:33:11 +0300

    set

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -33,25 +33,6 @@ const UsersController = {
     }
   },
 
-  getMe: async (req, res) => {
-    const { 'x-token': token } = req.headers;
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const userId = await redisClient.get(`auth_${token}`);
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const user = await dbClient.getUserById(userId);
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    return res.status(200).json({ id: user._id, email: user.email });
-  },
-
 };
 
 export default UsersController;
